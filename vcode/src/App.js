852749e6156625e72fd666232848270317116abf
Kkotiswar <lit2020044@iiitl.ac.in>
2022-04-23 04:53:35 +0530

    add imp folders

--- vcode/src/App.js
+++ vcode/src/App.js
@@ -1,10 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+import  NavbarTop  from './components/NavbarTop';
+import NavbarSec from './components/NavbarSec';
+import Caraousel from './components/Caraousel';
+import Auth from './components/forms/Auth';
+import About from './components/aboutus/about'
+
 
 function App() {
+
+  const [form,setForm] = useState(false);
+  // const [about, setAbout] = useState(false);
+
+  function handleBtnClick(){
+      setForm(prev =>{
+        return !prev;
+      })
+  }
+
+  // function handleClick(){
+  //   return 
+  // }
+  
   return (
+    
     <div className="App">
-        
+      
+     
+     <NavbarTop 
+       handleBtnClick={handleBtnClick}
+     />
+     { <NavbarSec
+       handleBtnClick={handleBtnClick}
+     />}
+
+    { form === true ? <Auth /> : <Caraousel />} 
+
+     <About/>
+      
     </div>
   );
 }
