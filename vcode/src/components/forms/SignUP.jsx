82701804ce309ccc959afc877231a58aeaa1c3fc
Kkotiswar <lit2020044@iiitl.ac.in>
2022-04-23 15:26:14 +0530

    Update SignUP.jsx

--- vcode/src/components/forms/SignUP.jsx
+++ vcode/src/components/forms/SignUP.jsx
@@ -72,13 +72,10 @@ const SignUp = () => {
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formGridAddress1">
-<<<<<<< Updated upstream
                         <Form.Label>CodeForces Handle</Form.Label>
                         <Form.Control required placeholder="CodeForces Handle" name='CfHandle' value={user.CfHandle} onChange={handleChange}/>
-=======
                         <Form.Label>Address</Form.Label>
                         <Form.Control required placeholder="1234 Main St" name='Address' value={user.Address} onChange={handleChange}/>
->>>>>>> Stashed changes
                     </Form.Group>
 
                     
